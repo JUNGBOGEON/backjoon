38c83a31d9575cf5c78ddfa01fd1c23e2c911060
Pyeonno <pyeonno13634@gmail.com>
2025-10-01 15:59:08 +0900

    회의실 배정

---
+++ 1931.py
@@ -0,0 +1,26 @@
+import sys
+
+class MeetingRoom:
+    def __init__(self):
+        self.rooms = []
+
+    def add_room(self, start, end):
+        self.rooms.append((start, end))
+
+    def max_schedule(self):
+        self.rooms.sort(key=lambda x: (x[1], x[0]))
+        count, last_end = 0, 0
+        for start, end in self.rooms:
+            if start >= last_end:
+                count += 1
+                last_end = end
+        return count
+
+n = int(sys.stdin.readline())
+room = MeetingRoom()
+
+for _ in range(n):
+    start, end = map(int, sys.stdin.readline().split())
+    room.add_room(start, end)
+
+print(room.max_schedule())
